39cbfec0b585b4a8c1252b3b08c57619ee8d0a76
Taylor Lehman <taylorlehman111@gmail.com>
2018-03-16 01:32:16 -0700

    Initial Commit

---
+++ DataStructures/DataStructures.h
@@ -0,0 +1,19 @@
+//
+//  DataStructures.h
+//  DataStructures
+//
+//  Created by Taylor Lehman on 3/16/18.
+//  Copyright © 2018 Taylor Lehman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DataStructures.
+FOUNDATION_EXPORT double DataStructuresVersionNumber;
+
+//! Project version string for DataStructures.
+FOUNDATION_EXPORT const unsigned char DataStructuresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataStructures/PublicHeader.h>
+
+
